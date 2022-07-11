ed37794db1635601a7d4e1f1c5fb85467fc1760b
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-07-11 10:22:19 -0700

    Loading Spinner Added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export default class News extends Component {
   constructor() {
@@ -12,66 +13,76 @@ export default class News extends Component {
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=385b3a8b1d7b4d13be9cfc4be7420e92&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=385b3a8b1d7b4d13be9cfc4be7420e92&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-    } else {
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=385b3a8b1d7b4d13be9cfc4be7420e92&page=${
         this.state.page + 1
-      }&pageSize=20`;
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
-      console.log(parsedData);
+
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
   handlePreviousClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=385b3a8b1d7b4d13be9cfc4be7420e92&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading: false,
     });
   };
 
   render() {
     return (
-      <div className="container my-3">
+      <div className="container my-3 text-center">
         <h2>Newsapp - Top Headlines</h2>
-
+        {this.state.loading && <Spinner />}
         <div className="row mt-5">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title.substr(0, 45)}
-                  description={
-                    element.description === null
-                      ? ""
-                      : element.description.substr(0, 100)
-                  }
-                  imgUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title.substr(0, 45)}
+                    description={
+                      element.description === null
+                        ? ""
+                        : element.description.substr(0, 100)
+                    }
+                    imgUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -83,6 +94,10 @@ export default class News extends Component {
             &#x3c; Previous
           </button>
           <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
             onClick={this.handleNextClick}
             type="submit"
             className="btn btn-dark"
---
+++ src/components/Spinner.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
+}
